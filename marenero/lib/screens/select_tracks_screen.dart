da0b85d58f2ff73342210e599b6c16182c8b340b
Mårten Walter <marwa361@student.liu.se>
2021-05-29 16:23:07 +0200

    feat: Select Track

--- marenero/lib/screens/select_tracks_screen.dart
+++ marenero/lib/screens/select_tracks_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:marenero/models/my_track.dart';
+import 'package:marenero/widgets/party_builder.dart';
 import 'package:marenero/widgets/select_tracks/search_tracks.dart';
 import 'package:marenero/widgets/select_tracks/selected_tracks_list.dart';
 
@@ -20,56 +21,68 @@ class SelectTracksScreen extends StatefulWidget {
 class _SelectTracksScreenState extends State<SelectTracksScreen> {
   List<MyTrack> selected = [];
 
+  callback(selectedTrack) {
+    setState(() {
+      selected.add(selectedTrack);
+      // TODO: Add to Firebase.
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
 
-    return Scaffold(
+    return PartyBuilder(
+      partyId: widget.partyId,
+      builder: (context, party) => Scaffold(
         appBar: AppBar(
           title: Column(
             children: [
               Text(
-                'code', // TODO: Fix me
+                party.code,
                 style: Theme.of(context).textTheme.headline1,
               ),
               Text(
-                '${1 + 1} party people', // TODO: Fix me
+                '${party.participants.length} party people',
                 style: Theme.of(context).textTheme.bodyText2,
               ),
             ],
           ),
         ),
         body: Container(
-            width: screenSize.width,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                // Selected tracks:
-                Column(
-                  children: [
-                    Text("SELECT YOUR BANGERS:"),
-                    Container(
-                      // TODO: Make this a StreamBuilder, or stateful.
-                      constraints: BoxConstraints(
-                        maxHeight: screenSize.height / 3,
-                      ),
-                      child: SelectedTracksList(tracks: selected),
+          width: screenSize.width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              // Selected tracks:
+              Column(
+                children: [
+                  Text("SELECT YOUR BANGERS:"),
+                  Container(
+                    // TODO: Make this a StreamBuilder, or stateful.
+                    constraints: BoxConstraints(
+                      maxHeight: screenSize.height / 3,
                     ),
-                  ],
-                ),
+                    child:
+                        SelectedTracksList(tracks: selected, songsToQueue: 3),
+                  ),
+                ],
+              ),
 
-                // Search tracks:
-                Container(
-                  constraints: BoxConstraints(
-                    maxHeight: screenSize.height / 3,
-                  ),
-                  child: SearchTracks(
-                    // TODO: Fix me
-                    spotifyAuthToken: 'widget.spotifyAuthToken',
-                    userid: widget.userId,
-                  ),
-                )
-              ],
-            )));
+              // Search tracks:
+              Container(
+                constraints: BoxConstraints(
+                  maxHeight: screenSize.height / 3,
+                ),
+                child: SearchTracks(
+                    spotifyAuthToken: party.spotifyToken,
+                    userid: "lostboy1",
+                    callback: callback),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
