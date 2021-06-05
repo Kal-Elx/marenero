d23c5ca946fcb198ee1e41b77fde674f08f85e2a
Kalle Johansson <kallej1johansson@gmail.com>
2021-06-05 15:47:56 +0200

    design: responsive for mobile

--- marenero/lib/screens/host_screen.dart
+++ marenero/lib/screens/host_screen.dart
@@ -101,9 +101,9 @@ class _HostScreenState extends State<HostScreen> {
                 userId: participant.id,
               ),
               floatingActionButtonLocation:
-                  FloatingActionButtonLocation.miniEndFloat,
+                  FloatingActionButtonLocation.miniEndTop,
               body: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
                 child: Column(
                   children: [
                     RoundedDivider(),
