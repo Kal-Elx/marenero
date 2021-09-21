9342d8c53904445ab400610a122900a54fc9a182
Kalle Johansson <kallej1johansson@gmail.com>
2021-09-21 20:19:22 +0200

    feat: safari error message

--- marenero/lib/screens/error_screen.dart
+++ marenero/lib/screens/error_screen.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:web_browser_detect/web_browser_detect.dart';
 
 class ErrorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final browser = Browser.detectOrNull();
+
     return Scaffold(
       appBar: AppBar(),
       body: Center(
         child: Text(
-          'Oh no, something went wrong',
+          browser?.browser.contains('Safari') ?? false
+              ? "Oh no, something went wrong and it's most likely because of your browser\nTry Google Chrome instead"
+              : 'Oh no, something went wrong',
+          textAlign: TextAlign.center,
           style: Theme.of(context).textTheme.headline2,
         ),
       ),
